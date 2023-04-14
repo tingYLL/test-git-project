8d5f7fed1393558f2de098b16de164bb1a8d6b14
tingYLL <130065441+tingYLL@users.noreply.github.com>
2023-04-14 21:20:48 +0800

    Update Leetcode72.java

--- untitled/src/Leetcode72.java
+++ untitled/src/Leetcode72.java
@@ -7,7 +7,7 @@ public class Leetcode72 {
     public static void main(String[] args) {
         System.out.println(new Leetcode72().minDistance("horse", "ros"));
         System.out.println("bbbbb");
-        System.out.println("ccccccc");
+   
 
     }
 
